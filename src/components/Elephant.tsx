3d3bae765388b4272daca906c9796ddd94cfe28a
Sabyrka <sabyrka18@outlook.com>
2025-12-17 19:44:13 +0500

    feat: v3

--- src/components/Elephant.tsx
+++ src/components/Elephant.tsx
@@ -1,7 +1,19 @@
+import { useState } from 'react'
+
 export const Elephant = () => {
+  const [weight, setWeight] = useState(100)
+
+  const handleFeedHealthyFood = () => setWeight(prev => prev + 20)
+  const handleFeedJunkFood = () => setWeight(prev => prev - 20)
+
   return (
-    <div style={{ fontSize: '100px' }}>
-      🐘
+    <div>
+      <h1>Покорми слона</h1>
+      <button onClick={handleFeedHealthyFood}>Кормить слона полезной едой 🥬🍉🍌</button>
+      <br />
+      <button onClick={handleFeedJunkFood}>Кормить слона вредной едой 🍔🍬🍕</button>
+      <div style={{ fontSize: `${weight}px` }}>🐘</div>
+      <button>Давай сыграем еще раз</button>
     </div>
   )
 }
